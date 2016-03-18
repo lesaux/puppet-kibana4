8b4355abc80fe131f700f1d69ef5ba668b452392
Gianni Giacchetta <gianni.giacchetta@vidzone.tv>
2016-03-18 16:04:04 +0000

    Minor refactoring to match lesaux's style

--- manifests/params.pp
+++ manifests/params.pp
@@ -18,7 +18,7 @@ class kibana4::params {
   $service_ensure                = true
   $service_enable                = true
   $service_name                  = 'kibana'
-  case $operatingsystem {
+  case ${::operatingsystem} {
     /^(Debian|Ubuntu)$/ : { $service_provider = debian }
     default:              { $service_provider = init   }
   }
