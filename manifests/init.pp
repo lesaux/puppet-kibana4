b4794a4a12ead9c6f56d7f07afed816ab020bfe4
Alex Harvey <Alex.Harvey@team.telstra.com>
2016-05-16 02:42:35 +1000

    Add feature manage_repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,9 +8,16 @@
 # Version of Kibana4 that gets installed.  Defaults to the latest version
 # available in the `package_repo_version` that is selected.
 #
+# [*manage_repo*]
+# Enable repo management by enabling the official repositories.
+#
 # [*package_repo_version*]
 # apt or yum repository version. Only used if 'package_use_official_repo' is set to 'true'.
 # defaults to '4.5'.
+#
+# [*package_repo_proxy*]
+# A proxy to use for downloading packages.
+# Defaults to 'undef'. You can change this if you are behind a proxy
 #
 # [*service_ensure*]
 # Specifies the service state. Valid values are stopped (false) and running
@@ -23,10 +30,6 @@
 # [*service_name*]
 # Name of the Kibana4 service. Defaults to 'kibana'.
 #
-# [*package_repo_proxy*]
-# A proxy to use for downloading packages.
-# Defaults to 'undef'. You can change this if you are behind a proxy
-#
 # [*babel_cache_path*]
 # Kibana uses babel (https://www.npmjs.com/package/babel) which writes it's cache to this location
 #
@@ -36,6 +39,7 @@
 #
 class kibana4 (
   $version                       = $kibana4::params::version,
+  $manage_repo                   = $kibana4::params::manage_repo,
   $package_repo_version          = $kibana4::params::package_repo_version,
   $package_repo_proxy            = undef,
   $service_ensure                = $kibana4::params::service_ensure,
@@ -44,6 +48,12 @@ class kibana4 (
   $config                        = $kibana4::params::config,
   $plugins                       = undef,
 ) inherits kibana4::params {
+
+  validate_bool($manage_repo)
+
+  if ($manage_repo) {
+    validate_string($package_repo_version)
+  }
 
   class {'kibana4::install': }->
   class {'kibana4::config': }->
@@ -55,5 +65,4 @@ class kibana4 (
     validate_hash($plugins)
     create_resources('kibana4::plugin', $plugins)
   }
-
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -5,6 +5,7 @@
 class kibana4::params {
   $babel_cache_path              = '/tmp/babel.cache'
   $version                       = 'latest'
+  $manage_repo                   = true
   $package_repo_version          = '4.5'
   $package_install_dir           = '/opt/kibana'
   $service_ensure                = true
