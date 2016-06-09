a30d898420a64b9819738d050210e8af473cdc5e
Aleksandar Pesic <peske.nis@gmail.com>
2016-06-09 04:04:34 +0200

    Cosmetics (indentation).

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,12 +6,12 @@ class kibana4::install {
 
   contain kibana4::install::package
 
-	file { 'kibana_plugin_install_proxy.sh': 
-	  ensure  => 'file', 
-	  path    => '/opt/kibana/kibana_plugin_install_proxy.sh', 
-	  mode    => '0755', 
-	  source  => 'puppet:///modules/kibana4/kibana_plugin_install_proxy.sh', 
+  file { 'kibana_plugin_install_proxy.sh': 
+    ensure  => 'file', 
+    path    => '/opt/kibana/kibana_plugin_install_proxy.sh', 
+    mode    => '0755', 
+    source  => 'puppet:///modules/kibana4/kibana_plugin_install_proxy.sh', 
     require => Class['kibana4::install::package'], 
-	}
+  }
 
 }
