efba8b4a5dae27b9bffdc7284229753196aaa178
Pmoranga <pmoranga@users.noreply.github.com>
2016-06-22 14:39:37 +0200

    Removed deprecated usage of user and group #62
    Fixes #62, where reference to username and group were still present. As a configuration file it should not be owned by the user running the application also.

--- manifests/config.pp
+++ manifests/config.pp
@@ -11,8 +11,6 @@ class kibana4::config {
     file { 'kibana-config-file':
       ensure  => file,
       path    => $_config_file,
-      owner   => $kibana4::kibana4_user,
-      group   => $kibana4::kibana4_group,
       mode    => '0755',
       content => template('kibana4/kibana.yml.erb'),
       notify  => Service['kibana4'],
