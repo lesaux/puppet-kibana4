10aa4c811cedf303a2d4825ddc5011db98de0730
Craig Genner <craig.genner@piksel.com>
2016-07-11 11:03:27 +0100

    need the alias to allow 'catalogue without dependency cycles' to pass

--- manifests/service.pp
+++ manifests/service.pp
@@ -8,6 +8,7 @@ class kibana4::service {
     ensure     => $kibana4::service_ensure,
     enable     => $kibana4::service_enable,
     name       => kibana,
+    alias      => kibana,
     provider   => $kibana4::service_provider,
     hasstatus  => true,
     hasrestart => true,
