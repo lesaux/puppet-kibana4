0bb67e803b15205dd6dec2cfc808ec760491c2a9
Tom Miller <tmiller@twcable.com>
2016-07-12 10:45:15 -0600

    Fixing file permissions on kibana.yml.

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,6 +5,8 @@
 
 class kibana4::config {
 
+  # This should really be fixed at the kibana package level. There is no reason not to have
+  # these files owned by Kibana as it makes installing plugins much more seamless.
   file { 'fix-optimize-bundles':
     ensure  => directory,
     path    => '/opt/kibana/optimize',
@@ -18,7 +20,7 @@ class kibana4::config {
     path    => '/opt/kibana/config/kibana.yml',
     owner   => $kibana4::kibana4_user,
     group   => $kibana4::kibana4_group,
-    mode    => '0755',
+    mode    => '0644',
     content => template('kibana4/kibana.yml.erb'),
     notify  => Service['kibana4'],
   }
