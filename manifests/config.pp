c666c88710fe759638d2248470876549ce972c8e
John Cooper <john@choffee.co.uk>
2017-02-12 22:37:11 +0000

    Fix config location

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,7 +8,7 @@ class kibana::config {
 
     file { 'kibana-config-file':
       ensure  => file,
-      path    => '/opt/kibana/config/kibana.yml',
+      path    => '/etc/kibana/config/kibana.yml',
       owner   => 'kibana',
       group   => 'kibana',
       mode    => '0755',
